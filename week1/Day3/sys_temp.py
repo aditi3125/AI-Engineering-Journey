07e518d3091ef65fc66c0cb592f8a575903cbd00
Aditi Kumari <aditikumari312004kv@gmail.com>
2026-07-28 02:34:56 +0530

    Add Week 1 Day 3 - System Role and Temperature

---
+++ week1/Day3/sys_temp.py
@@ -0,0 +1,33 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+if not my_api_key:
+    raise ValueError("api error")
+
+client =Groq(api_key=my_api_key)
+model="llama-3.3-70b-versatile"
+role="user"
+prompt="I Love you "
+message_system={
+    "role":"system",
+    "content":"You are my Strict Teacher"
+}
+message={
+    "role":role,
+    "content":prompt
+}
+messages=[message_system,message]
+response=client.chat.completions.create(model=model,messages=messages,temperature=2)
+
+##print(response)
+
+print("***********************************************************************************************************************************************")
+
+answer=response.choices[0].message.content
+print(answer)
+
+print("***********************************************************************************************************************************************")
